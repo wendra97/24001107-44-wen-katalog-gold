0e0c3ab5ddba2372ae44cc338503c3e35af48b01
Wendra Setiawan <setiawanwendra97@gmail.com>
2024-05-26 00:19:48 +0700

    Initialize express, server, and create landing page

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
