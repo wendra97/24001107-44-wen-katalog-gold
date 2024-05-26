849053ff638677d2eb49c56d471e71ba2c72e674
Wendra Setiawan <setiawanwendra97@gmail.com>
2024-05-26 23:48:57 +0700

    Create endpoint to get all users, specific user, and add user

---
+++ database/database.js
@@ -0,0 +1,5 @@
+const knex = require("knex");
+const knexfile = require("../knexfile.js");
+const db = knex(knexfile.development);
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const knex = require("knex");
+const db = require("./database/database.js");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await db("users").select("*");
+  return res.json({ data: users });
+});
+
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await db("users").select("*").where({ user_id: id }).first();
+  return res.json({ data: user });
+});
+
+app.post("/users", async (req, res) => {
+  const { name, email, username, password } = req.body;
+  const createdAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
+  const user = await db("users").insert(
+    {
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      created_at: createdAt,
+      updated_at: updatedAt,
+    },
+    ["name", "email", "username", "password"]
+  );
+  return res.json({ message: "User successfully created", data: user });
 });
 
 app.listen(port, () => {
